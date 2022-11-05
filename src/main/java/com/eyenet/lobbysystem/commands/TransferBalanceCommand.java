0baebca6c50a2d67d9809ecf3f346a43f954b075
EyeDe7 <noa.bamberg@protonmail.com>
2022-11-05 01:40:48 +0100

    Tranfer Money completed

--- src/main/java/com/eyenet/lobbysystem/commands/TransferBalanceCommand.java
+++ src/main/java/com/eyenet/lobbysystem/commands/TransferBalanceCommand.java
@@ -7,7 +7,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Objects;
 import java.util.UUID;
 
 public class TransferBalanceCommand implements CommandExecutor {
